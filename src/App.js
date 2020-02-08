f167e2f069f8392c5dfbd710961ba98a27d968e1
Franz Noel Tanglao <franz@tanglao.org>
2020-02-07 23:17:53 -0800

    #1 feat: Create members table

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import MembersTable from './components/MembersTable';
 
 function App() {
   return (
     <div className="App">
+      <MembersTable></MembersTable>
     </div>
   );
 }
